beaaa3e78c66cd97939a5a6225683332262190e5
renanetec <renanmenezescarlos21@gmail.com>
2021-03-12 19:31:41 -0300

    Altera todo o inicio

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Contagem Regressiva!");
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ReadKey();
+            Console.WriteLine("É 1 ...");
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ReadKey();
+            Console.WriteLine("É 2 ...");
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ReadKey();
+            Console.WriteLine("É 3 ...");
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.ReadKey();
+            Console.WriteLine("E já!");
+            Console.WriteLine();
+            Console.ResetColor();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ContagemRegressiva
-Faça um programa que exiba u ma contagem regressiva no seguinte formato: Exercício
+# ContagemRegressiva - Exercício
+Faça um programa que exiba uma contagem regressiva no seguinte formato: Exercício
+Preparar...
+Apontar...
+FOGO!
+OBS: Aguarde o pressionamento de uma tecla antes de cada transição.
+
+# nome: Renan de Oliveira Ferreira Menezes
+
+
